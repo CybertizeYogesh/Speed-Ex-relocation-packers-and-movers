1504159db73ce89ef6b1855f575ee5f43817c95d
CybertizeYogesh <cybertizeyogesh@gmail.com>
2026-07-20 13:18:12 +0530

    Update website logo to new speed.png

--- src/components/Header.js
+++ src/components/Header.js
@@ -37,7 +37,7 @@ export default function Header() {
             <div className="container">
               {/* Logo Start */}
               <Link className="navbar-brand" href="/">
-                <img src="/sppedlogo.png" style={{ background: "#fff", borderRadius: "10px", maxWidth: "300px" }} alt="Speed Ex Relocation Logo" />
+                <img src="/global-logo.png" style={{ background: "#fff", borderRadius: "10px", maxWidth: "300px" }} alt="Logo" />
               </Link>
               {/* Logo End */}
 
